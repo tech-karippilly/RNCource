0b375b476da2fa4bec9a41f7df26d241495a8df1
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-06-20 22:39:10 +0530

    app set up

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput, View } from 'react-native';
+
+function StartGameScrenn(){
+    return(
+        <View>
+            <TextInput/>
+        </View>
+    )
+}
+
+export default StartGameScrenn;
